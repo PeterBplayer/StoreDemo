b01f24e1fedeec1f3df70832bd3faf94bec06f7c
Piotr Keppel <piotr.keppel@soflab.pl>
2023-02-06 16:04:17 +0100

    Refactor HomePage.java Add ProductListPage, ProductPage & CheckOutTest

---
+++ src/test/java/pl/soflab/tests/CheckOutTest.java
@@ -0,0 +1,16 @@
+package pl.soflab.tests;
+
+import org.testng.annotations.Test;
+import pl.soflab.pages.HomePage;
+
+public class CheckOutTest extends BaseTest {
+
+    @Test
+    public void checkoutTest() {
+
+        new HomePage(driver)
+                .openShopPage()
+                .openProduct("Java Selenium WebDriver")
+                .addProductToCart();
+    }
+}
